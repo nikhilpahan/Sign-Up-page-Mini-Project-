26e31aa5372aa9a84dd6278e4f531d0faa2141e4
nikhilpahan <nikhilpahan02@gmail.com>
2022-09-03 01:37:59 +0530

    Unable to Understand JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const container=document.querySelector(".container");
+const signupBtn=document.querySelector(".green-bg button");
+
+signupBtn.addEventListener("click",() => {
+    container.classList.toggle("change")
+    ;
+});
